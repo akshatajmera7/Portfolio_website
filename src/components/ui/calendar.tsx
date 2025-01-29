8c394cdf94077b20bfa93a25f658992733ce5dab
akshatajmera7 <akshatajmera777@gmail.com>
2025-01-29 17:59:39 +0530

    fixed minor bug

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -57,11 +57,11 @@ function Calendar({
         day_hidden: 'invisible',
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
-      }}
-      {...props}
+      // components={{
+      //   IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
+      //   IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+      // }}
+      // {...props}
     />
   );
 }
